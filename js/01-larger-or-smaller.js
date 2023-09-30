07ad620ac95b3e5fdc0fbb401dbe39cfe845d1a9
jcpimentel85 <144981792+jcpimentel85@users.noreply.github.com>
2023-09-30 11:33:24 -0700

    first comming

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,24 @@
+let varnum1= prompt('Enter 1st number')
+let varnum2= prompt('Enter 2nd number')
+Number(varnum1)
+Number(varnum2)
+console.log(varnum1)
+console.log(varnum2)
+console.log(Boolean (varnum1 > varnum2))
+if (varnum1 != varnum2) { 
+    if (varnum1 > varnum2) {alert(`The larger number is ${varnum1}`)
+        } else {alert(`The larger number is ${varnum2}`)
+    } 
+}else {alert(`Error in the Matrix`)
+}
+
+//    if (varnum1 != varnum2) { alert('Numbers are the same')
+//}else if (varnum1 > varnum2) {alert(`The larger number is ${varnum2}`)
+//else if (num1 < num2) {alert(`The larger number is ${num2}`)}
+//}else {alert(`Error in the Matrix`)
+//}
+
+
+
+    //   else if (num1 > num2) {alert(`The larger number is` ${num1})}
+ //       else  {alert(`The larger number is` ${num2})}
