ab751dc7fd69527034633cedce1a279410cd0c8f
jcpimentel85 <jcpimentel_85@hotmail.com>
2023-09-30 15:50:39 -0700

    coinflip changes

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,5 +1,13 @@
+//Usage: Use simple conditional statements
+//Create an application that accepts two integers within two separate prompts. 
+//Then, display only the larger of the two within the browser window. Don’t forget 
+//to handle the fact that the two could be equal.
+
+//Create Variables and propmt for values
 let varNum1= parseInt(prompt('Enter 1st number'))
 let varNum2= parseInt(prompt('Enter 2nd number'))
+
+//Conditional Access
 if (varNum1 != varNum2) { 
     if (varNum1 > varNum2) {alert(`The larger number is ${varNum1}`)
         } else {alert(`The larger number is ${varNum2}`)
---
+++ js/03-coinflip.js
@@ -0,0 +1,42 @@
+//The “Coin Flip” Game
+//Usage: Use nested conditional statements
+//In this part of the assignment you will explore what you’ve learned so far about variables, prompts, 
+//conditionals, operators, math, and more to create a “coin flip game”. To complete the game 
+//follow the steps outlined below:
+//Begin your application by creating a variable called coinFlip and set it equal to a random number. 
+//You will have to use a Math method to get this number:
+//let randomNum = Math.round(Math.random())
+//Prompt the user to select “Heads or Tails” and set the result to a new variable called choice.
+//Use a conditional to check the result of the coin flip. If it’s less than a certain number, 
+//it will be heads. If it’s greater than a certain number, it will be tails.
+//If the result is heads and the user selects heads, display the following message within an alert:
+//The flip was heads and you chose heads...you win!
+//If the result is heads and the user selects tails, display the following message within an alert:
+//The flip was heads but you chose tails...you lose!
+//If the result is tails and the user selects heads, display the following message within an alert:
+//The flip was tails but you chose heads...you lose!
+//If the result is tails and the user selects tails, display the following message within an alert:
+//The flip was tails and you chose tails...you win!
+
+// Create variables
+let coinFlip = Math.round(Math.random())
+let choice= prompt("Select Heads or Tails").toLowerCase()
+console.log(coinFlip)
+console.log(choice)
+//console.log (choice = "Heads")
+console.log (choice == "heads")
+console.log (choice === "heads")
+// Nested Conditional Statements
+if ( choice == "heads" || choice == "tails") {
+    if (coinFlip >= 1) { 
+        if ( choice == "heads") {document.write(`The flip was heads and you chose heads...you win`)
+            } else {document.write(`The flip was heads and you chose tails...you lose`)
+        } 
+    }else {
+        if ( choice == "heads") {document.write(`The flip was tails and you chose heads...you lose`)
+            } else {document.write(`The flip was tails and you chose tails...you win`)
+            }
+        }
+    }   
+else {document.write("Try again, only enter Heads or Tails, other values will show this error")}
+
